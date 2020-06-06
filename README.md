14336343d098dc11d1ebee40874ba49098504d25
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2020-06-06 18:44:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# YAM
+
+**Requires [DataManager](https://github.com/tes3mp-scripts/DataManager)!**
+
+Yet Another MOTD script for TES3MP.
+
+## Configuration
+
+* `btnText`
+
+String.  Text to show for the confirmation button.  Default: `OK`
+
+* `chatOnly`
+
+Boolean.  Whether or not to only print the MOTD in the chat box.  Default: `false`
+
+* `motd`
+
+String.  The MOTD to print.  Default: `#008000This is the default MOTD for YAM!\n\nYou should change it.#FFFFFF`
+
+* `showInChat`
+
+Boolean.  Whether or not to also print the MOTD in the chat box.  Default: `false`
---
+++ main.lua
@@ -0,0 +1,26 @@
+local YAM = {}
+
+local scriptName = "YAM"
+
+YAM.defaultConfig = {
+    btnText = "OK",
+    chatOnly = false,
+    motd = color.Green .. "This is the default MOTD for YAM!\n\nYou should change it." .. color.Default,
+    showInChat = false,
+}
+
+YAM.config = DataManager.loadConfiguration(scriptName, YAM.defaultConfig)
+
+YAM.print = function(eventStatus, pid)
+	if eventStatus.validDefaultHandler then
+        if YAM.config.chatOnly or YAM.config.showInChat then
+            tes3mp.SendMessage(pid, YAM.config.motd .. "\n")
+        end
+        if not YAM.config.chatOnly then
+            tes3mp.CustomMessageBox(pid, -1, YAM.config.motd, YAM.config.btnText)
+        end
+    end
+end
+
+customEventHooks.registerHandler("OnPlayerEndCharGen", YAM.print)
+customEventHooks.registerHandler("OnPlayerFinishLogin", YAM.print)
